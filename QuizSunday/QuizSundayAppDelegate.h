5ed5bd84cef431743b290a698b0b1e4e5ecc576c
Richard Barrett-Small <rickbs@gmail.com>
2012-03-04 14:29:46 +0000

    Initial Commit

---
+++ QuizSunday/QuizSundayAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QuizSundayAppDelegate.h
+//  QuizSunday
+//
+//  Created by Richard Barrett-Small on 04/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizSundayAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
